eb8f28fcb601057aa6681c7c1065ad1ff6aedbf2
Michael Famurewa <54787734+viscountfam@users.noreply.github.com>
2019-12-09 16:19:35 -0500

    Finished MVP and some stretch
    I finished MVP and i'm trying for some stretch goals but the doc doesn't look as good i'd like it to.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
-
+// import {setquarter} from "./BottomRow"
+// import {quarter} from "./BottomRow"
+// import {setdown} from "./BottomRow"
+// import {down} from "./BottomRow"
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [lionsScore, lionsScoreSetter] = useState(0);
@@ -29,12 +32,32 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick= {() => lionsScoreSetter(lionsScore + 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick= {() => lionsScoreSetter(lionsScore + 3)}>Home Field Goal</button>
+          <button
+            className="homeButtons__touchdown"
+            onClick={() => lionsScoreSetter(lionsScore + 7)}
+          >
+            Home Touchdown
+          </button>
+          <button
+            className="homeButtons__fieldGoal"
+            onClick={() => lionsScoreSetter(lionsScore + 3)}
+          >
+            Home Field Goal
+          </button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick= {() => tigerScoreSetter(tigerScore + 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick= {() => tigerScoreSetter(tigerScore + 3)}>Away Field Goal</button>
+          <button
+            className="awayButtons__touchdown"
+            onClick={() => tigerScoreSetter(tigerScore + 7)}
+          >
+            Away Touchdown
+          </button>
+          <button
+            className="awayButtons__fieldGoal"
+            onClick={() => tigerScoreSetter(tigerScore + 3)}
+          >
+            Away Field Goal
+          </button>
         </div>
       </section>
     </div>
